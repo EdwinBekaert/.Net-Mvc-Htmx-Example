ab4396309397e202f03e880eab4fe7f1bfe289c2
Edwin Bekaert <edwin.bekaert@e2e.be>
2023-11-14 13:57:30 +0100

    calculator result partial + viewmodel + InputNumber endpoint

--- Calculator.WebApp/Features/Calculator/CalculatorController.cs
+++ Calculator.WebApp/Features/Calculator/CalculatorController.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Calculator.App;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Calculator.WebApp.Features.Calculator;
 
+[Route("Calculator")]
 public class CalculatorController : Controller
 {
+    private readonly ICalculator _calc;
+
+    public CalculatorController(IHttpContextAccessor httpContextAccessor) 
+        => _calc = new SessionCalculator(httpContextAccessor);
+
     public IActionResult Index()
     {
-        return View();
+        var viewModel = new CalculatorViewModel(_calc);
+        return View(viewModel);
+    }
+
+    [HttpGet]
+    [Route("InputNumber/{inputValue:int}")]
+    public IActionResult InputNumber([FromRoute]int inputValue)
+    {
+        _calc.InputNumber(inputValue);
+        var viewModel = new CalculatorViewModel(_calc);
+        return PartialView("_CalculatorResult", viewModel);
     }
 }
---
+++ Calculator.WebApp/Features/Calculator/CalculatorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Calculator.App;
+
+namespace Calculator.WebApp.Features.Calculator;
+
+public class CalculatorViewModel
+{
+    public CalculatorViewModel(ICalculator calc)
+    {
+        Digits = App.Calculator.Digits;
+        ActiveValue = calc.ActiveValue;
+        ResultValue = calc.ResultValue;
+    }
+    public int[] Digits { get; set; }
+    public decimal ActiveValue { get; set; }
+    public decimal ResultValue { get; set; }
+}

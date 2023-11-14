e34bc74967f0156acc59645b2c0c702eb3d4fb4f
Edwin Bekaert <edwin.bekaert@e2e.be>
2023-11-14 13:55:44 +0100

    fix partialview for rider

---
+++ Calculator.WebApp/RiderFix.cs
@@ -0,0 +1,18 @@
+﻿/*
+    This file exists to resolve some files in Rider IDE.
+ */
+
+using JetBrains.Annotations;
+
+[assembly: AspMvcViewLocationFormat("~/Features/Shared/{0}.cshtml")]
+[assembly: AspMvcPartialViewLocationFormat("~/Features/Shared/{0}.cshtml")]
+[assembly: AspMvcViewLocationFormat("~/Features/{1}/{0}.cshtml")]
+[assembly: AspMvcPartialViewLocationFormat("~/Features/{1}/{0}.cshtml")]
+[assembly: AspMvcMasterLocationFormat("~/Features/{1}/{0}.cshtml")]
+
+namespace Calculator.WebApp;
+
+public class RiderFix
+{
+    
+}

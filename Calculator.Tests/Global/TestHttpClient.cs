f8a8f61e4fc521cebd5f28bb310e3b5cc1f98f7b
Edwin Bekaert <edwin.bekaert@e2e.be>
2023-11-14 13:17:00 +0100

    cleanup

--- Calculator.Tests/Global/TestHttpClient.cs
+++ Calculator.Tests/Global/TestHttpClient.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Calculator.Tests.Global;
 
